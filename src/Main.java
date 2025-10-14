29ae5e4c30c6d5def677fe8be7891597fa06e0aa
EvanBurman <evanb77@nycstudents.net>
2025-10-14 11:40:25 -0400

    WordMatch

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        WordMatch game = new WordMatch("concatenation");
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess("nation"));
+        System.out.println(game.findBetterGuess("ten", "nation"));
+        System.out.println(game.scoreGuess("con"));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con", "cat"));
+
+    }
+
+}
