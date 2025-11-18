bcad7b0431cb5ef63962ed799fec3a824991da9d
EvanBurman <evanb77@nycstudents.net>
2025-11-18 11:19:07 -0500

    WordMatch Tasks complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        System.out.println();
+        WordMatch a = new WordMatch("aaaabb");
+        System.out.println(a.scoreGuess("a"));
+        System.out.println(a.scoreGuess("aa"));
+        System.out.println(a.scoreGuess("aaa"));
+        System.out.println(a.scoreGuess("aabb"));
+        System.out.println(a.scoreGuess("c"));
+        System.out.println();
         WordMatch game = new WordMatch("concatenation");
         System.out.println(game.scoreGuess("ten"));
         System.out.println(game.scoreGuess("nation"));
@@ -7,6 +20,8 @@ public class Main {
         System.out.println(game.scoreGuess("con"));
         System.out.println(game.scoreGuess("cat"));
         System.out.println(game.findBetterGuess("con", "cat"));
+
+
 
     }
 
